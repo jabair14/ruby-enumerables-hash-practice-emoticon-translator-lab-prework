9b176bb8692c05d0d6e4207432cafcc536cfb03f
Jabair Khan <jabairk@gmail.com>
2021-02-10 22:52:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,19 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticon_hash = {}
+ binding.pry
+
+
 end
 
 def get_japanese_emoticon
-  # code goes here
+
+  
 end
+
 
 def get_english_meaning
   # code goes here
