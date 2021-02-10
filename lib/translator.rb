88f331d6f3cbcbcee513f0d0f9989ab39309792c
Jabair Khan <jabairk@gmail.com>
2021-02-10 23:52:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,8 +19,10 @@ def get_japanese_emoticon(path, emoticon)
   load_library(path).each do |key, value|
     if value[:english] == emoticon
       return value[:japanese]
+      
+    end
     
-    binding.pry 
+    
   end
 
   
