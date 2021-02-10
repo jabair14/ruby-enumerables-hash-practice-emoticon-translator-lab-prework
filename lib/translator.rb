2c8bf673a7bc8a8782411bc46a06bed96dc75485
Jabair Khan <jabairk@gmail.com>
2021-02-10 23:01:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'pry'
 def load_library(path)
   emoticons = YAML.load_file(path)
   emoticon_hash = {"emoticon_name"=> {}, "emoticon_meaning"=>{}}
-  
+  path.each do |key, value|
  binding.pry
 
 
